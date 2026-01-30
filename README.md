762b7571e1a8a234113d8858d1cac27ca20c9f64
gcasado0 <casado.gustavo@gmail.com>
2026-01-30 14:05:46 -0300

    Adaptacion para que corra local

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# deeplearning
+# Automatización con IA
+
+# crear enviroment
+```bash
+# Crear entorno virtual (opcional pero recomendado)
+python3 -m venv .venv
+source .venv/bin/activate  # En Windows: venv\Scripts\activate
+```
+
+# Instalar dependencias
+```bash
+pip install -r requirements.txt
+```
---
+++ lib_openai.py
@@ -0,0 +1,30 @@
+# Función para generar el archivo jsonl de requests para batch
+
+import pandas as pd
+import json
+import configparser
+import numpy as np
+import openai
+from pathlib import Path
+import re
+
+# Leer config.ini
+config = configparser.ConfigParser()
+config_path = Path(__file__).parent / "config.ini"
+config.read(config_path)
+
+
+OPENAI_API_KEY = config["OPENAI"]["API_KEY"]
+openai.api_key = OPENAI_API_KEY
+
+def get_completion_from_messages(messages, 
+                                 model="gpt-3.5-turbo",
+                                 temperature=0,
+                                 max_tokens=500):
+    response = openai.chat.completions.create(
+        model=model,
+        messages=messages,
+        temperature=temperature, 
+        max_tokens=max_tokens,
+    )
+    return response.choices[0].message.content.strip()
